722676c6e1fb1e42aab6c84db5308cc06c8bf3c5
rider7 <brandon.rider@gmail.com>
2016-07-18 09:57:42 -0600

    set up ui-router and tested routes for 5 views successfully

---
+++ app.js
@@ -0,0 +1,34 @@
+angular.module('karSync', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider
+    .state('index', {
+      url: '/'
+    })
+    .state('dashboard', {
+    templateUrl: './views/dashboard.html',
+    url: '/dashboard/',
+    controller: 'dashCtrl'
+    })
+    .state('addCust', {
+      templateUrl: './views/addCustomer.html',
+      url: '/add/',
+      controller: 'addCtrl'
+    })
+    .state('currentCust', {
+      templateUrl: './views/currentCustomer.html',
+      url: '/current/',
+      controller: 'currentCtrl'
+    })
+    .state('login', {
+      templateUrl: './views/login.html',
+      url: '/login/',
+      controller: 'loginCtrl'
+    })
+    .state('maintenance', {
+      templateUrl: './views/maintenance.html',
+      url: '/maintenance/',
+      controller: 'maintCtrl'
+    })
+  $urlRouterProvider
+  .otherwise('/')
+});
