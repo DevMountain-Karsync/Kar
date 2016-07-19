0f30264ea21577495a7b63c58bc430c6c7d08e74
nicholas major <major.nicholas1@gmail.com>
2016-07-19 15:33:39 -0600

    style changes and magnifying glass hide

---
+++ js/directives/dashboardDirective.js
@@ -0,0 +1,17 @@
+angular.module('karSync')
+.filter('searchUserList', function(){
+  // console.log('dash user test');
+  return function(arr, searchUser){
+    if(!searchUser){
+      return arr;
+    }
+    var result = [];
+    searchUser = searchUser.toLowerCase();
+    angular.forEach(arr, function(item){
+      if(item.name.toLowerCase().indexOf(searchUser) !== -1){
+        result.push(item);
+    }
+    });
+    return result;
+}
+});
