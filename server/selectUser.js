4db8cf67c82bf516eede4a206ca89f1280d81e6e
Cameron Gale <cameroneldongale@gmail.com>
2016-07-19 11:46:17 -0600

    remade the get user by id endpoint to model the rest of the endpoints that we would need

--- server/selectUser.js
+++ server/selectUser.js
@@ -1,22 +1,29 @@
 
-var mysql = require('mysql');
-
-var config = require('./config.js')
-
-var connection = mysql.createConnection({
-  host     : config.host,
-  user     : config.user,
-  password : config.password,
-});
-connection.connect();
+// var mysql = require('mysql');
+//
+// var config = require('./config.js')
+//
+// var connection = mysql.createConnection({
+//   host     : config.host,
+//   user     : config.user,
+//   password : config.password,
+//   database: config.database,
+// });
+// connection.connect();
 
 
 // website.com/articles?id=1
 // website.com/articles?id=1; drop table articles;
 
-var id = '1';
+// var id = 'de39d4ab-4d22-11e6-9e98-0680f03a8445';
 
-var query = connection.query('select * from articles where id = ?',id, function(err, result) {
-  console.log(query.sql);
-  console.log(result);
-});
+exports.queryUser = function(req, res){
+      req.getConnection(function(err,connection){
+        connection.query('select * from user where id = ?', req.params.id, function(err, result) {
+          // console.log(err);
+          // console.log(result);
+          res.send(result)
+        })
+  })
+
+}
