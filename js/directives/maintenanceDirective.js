f57d3a6c8c7705e26c3ec208498dc7ad7483f688
nicholas major <major.nicholas1@gmail.com>
2016-07-19 18:00:00 -0600

    search for users on dashboard

--- js/directives/maintenanceDirective.js
+++ js/directives/maintenanceDirective.js
@@ -1,6 +1,7 @@
 angular.module('karSync')
 .filter('searchFor', function(){
     return function(arr, searchString){
+//make
         if(!searchString){
             return arr;
         }
@@ -8,11 +9,13 @@ angular.module('karSync')
         searchString = searchString.toLowerCase();
         angular.forEach(arr, function(item){
             if(item.name.toLowerCase().indexOf(searchString) !== -1){
+            // console.log(item)
             result.push(item);
         }
         });
         return result;
     };
+//model
     return function(arr, searchString1){
     if(!searchString1){
         return arr;
@@ -25,6 +28,22 @@ angular.module('karSync')
     }
     });
     return result;
-  }
+  };
 
+
+//users
+//   return function(arr, searchString2){
+//   if(!searchString2){
+//       return arr;
+//   };
+//   var result = [];
+//   searchString2 = searchString2.toLowerCase();
+//   angular.forEach(arr, function(item){
+//     console.log(item)
+//       if(item.name.toLowerCase().indexOf(searchString2) !== -1){
+//       result.push(item);
+//   }
+// });
+//   return result;
+// };
 });
