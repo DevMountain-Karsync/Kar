451cb04cae7317bace920592bde582dc25fc2a15
rider7 <brandon.rider@gmail.com>
2016-07-25 16:43:48 -0600

    first attempt at auto populating data into form based on clicked user

--- js/controllers/addCustomerController.js
+++ js/controllers/addCustomerController.js
@@ -1,15 +1,13 @@
 angular.module('karSync')
-.controller('addCtrl', function($scope, $http, userServ, partner){
+.controller('addCtrl', function($scope, $http, userServ, partner, vehicleService){
 
   $scope.partner = partner[0].partner_id;
   userServ.getUser($scope.partner).then(function(user){
     $scope.userList = user;
   })
 
-
+  //adds business_name to the scope based on partner authentication object
   $scope.business = partner[0].business_name;
-
-
 
   $scope.user = {};
   //adds partner_id from logged in partner to hidden field on customer form
@@ -18,6 +16,14 @@ angular.module('karSync')
   $scope.processForm = function(user){
     userServ.postUser(user)
     .then (function(res) {
+      //clears form data after sending
+        $scope.user.first_name = null;
+        $scope.user.phone = null;
+        $scope.user.first_name = null;
+        $scope.user.email = null;
+        $scope.user.address = null;
+        $scope.user.city = null;
+        $scope.user.postal_code = null;
       // console.log(res)
     })
   }
@@ -35,16 +41,12 @@ angular.module('karSync')
 
   //saved customer alert
   $scope.savedAlert = function(){
-  //clears form data after sending
-    $scope.user.first_name = null;
-    $scope.user.phone = null;
-    $scope.user.first_name = null;
-    $scope.user.email = null;
-    $scope.user.address = null;
-    $scope.user.city = null;
-    $scope.user.postal_code = null;
     //alert customer that customer saved successfully
     alert('Customer Saved!')
   }
 
+    $scope.customerClicked = function(user){
+      console.log("this is the user" + user.first_name)
+      $scope.user.first_name = user.first_name;
+  }
 });
--- js/controllers/newCustomerController.js
+++ js/controllers/newCustomerController.js
@@ -5,65 +5,13 @@ angular.module('karSync')
     $scope.userList = user;
   })
 
-
   $scope.business = partner[0].business_name;
-
-  userServ.getUser().then(function(user){
-    $scope.userList = user;
-  })
 
     $scope.select = function(item) {
       $scope.selected = item;
-
     }
 
     $scope.isActive = function(item) {
-
       return $scope.selected === item;
-
     }
-  $scope.customerClicked = function(user){
-    // console.log(user);
-    console.log('clicked '+ user.first_name);
-  vehicleService.getCar(user.account_id).then(function(res){
-
-
-
-    // console.log(res);
-    var array = res;
-
-    for (var i = 0; i < array.length; i++) {
-      if (array[i].vin === user.primary_vehicle) {
-        array.unshift(array[i])
-        array.splice(i,1)
-      }
-    }
-
-
-    $scope.data = {
-      vehicles: array,
-      selectedCar: 0,
-    }
-
-  })
-
-    // this will need changes once db more flushed out
-      user.primaryAccount = true
-      $scope.user = user;
-      $scope.user.primary_phone = "(801)-999-8888"
-      $scope.user.secondary_phone = "(801)-915-1515"
-      $scope.user.streetAddress = "12345 South State"
-      $scope.user.cityAndState = "Salt Lake City, UT 84020"
-
-
-      if (user.primaryAccount === true) {
-        $scope.primaryUser = user;
-      }
-
-      $scope.user.userPlan = "Gold"
-      $scope.user.userPlanPrice = "15.00"
-
-  }
-
-
 });
