1e73d5b3c04bc50afa74772843a581adfbb683c6
nicholas major <major.nicholas1@gmail.com>
2016-07-25 11:37:11 -0600

    logout button works

--- js/controllers/addCustomerController.js
+++ js/controllers/addCustomerController.js
@@ -1,5 +1,5 @@
 angular.module('karSync')
-.controller('addCtrl', function($scope, $http, userServ){
+.controller('addCtrl', function($scope, $http, userServ, partner){
   $scope.partner = partner[0].partner_id;
   userServ.getUser($scope.partner).then(function(user){
     $scope.userList = user;
