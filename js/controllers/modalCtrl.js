052a44d693a90b706f55f7baeef35c3239646bd4
rider7 <brandon.rider@gmail.com>
2016-07-26 11:47:07 -0600

    created form for add car modal

--- js/controllers/modalCtrl.js
+++ js/controllers/modalCtrl.js
@@ -1,6 +1,10 @@
 angular.module('karSync')
 .controller('modalCtrl', ['$scope', 'close', function($scope, close) {
 
+  $scope.savedAlert = function(){
+    //alert customer that customer saved successfully
+    alert('Car Saved!')
+  }
   $scope.close = close;
 
 }]);
