f5075a340dcbbe496d0912a1d47187f70c6db2de
nicholas major <major.nicholas1@gmail.com>
2016-07-29 15:39:16 -0600

    faster search for diagnostic look up, with pagination

---
+++ js/directives/paginationDirective.js
@@ -0,0 +1,15 @@
+// angular.module('karSync')
+// .filter('startFrom', function(){
+//   var input = $scope.test;
+//   return function (input, start, pageSize) {
+//         start = +start;
+//         pageSize = +pageSize;
+//         while (start > input.length) {
+//             start -= pageSize;
+//         }
+//         if (start < 0) {
+//             start = 0;
+//         }
+//         return input.slice(start);
+//     };
+// });
